06a9e13b50308b7a5861e443480de487d9ebcae3
Fedor <f.e.pivovarov@gmail.com>
2025-04-24 20:32:17 +0700

    parallel sources downloading

--- src/main/java/nsu/ru/pipeline/Downloader.java
+++ src/main/java/nsu/ru/pipeline/Downloader.java
@@ -5,19 +5,14 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.ResetCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.TransportException;
-
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
 import org.apache.commons.io.FileUtils;
-import java.util.regex.*;
 
 public class Downloader {
     public static Set<String> setupStudentsRepositories(List<String> repoUrls) throws DSLException {
@@ -27,86 +22,70 @@ public class Downloader {
             reposDir.mkdirs();
         }
 
-        for (String repoUrl : repoUrls) {
-            boolean success = false;
-            int attempts = 3;
-            Exception lastError = null;
+        repoUrls.parallelStream().forEach(repo -> {
+            setupStudentRepository(repo, subDirs, reposDir);
+        });
 
-            while (!success && attempts > 0) {
-                try {
-                    String[] parts = repoUrl.split("/");
-                    String studentNick = parts[parts.length - 2];
-                    String localRepoDirName = studentNick;
-                    File repoDir = new File(reposDir, localRepoDirName);
-                    subDirs.add("./repos/" + localRepoDirName);
-                    // Проверяем, существует ли репозиторий
-                    if (repoDir.exists() && new File(repoDir, ".git").exists()) {
-                        try (Git git = Git.open(repoDir)) {
-                            System.out.println("Pulling repository: " + repoDir.getName());
+        return subDirs;
+    }
+
+    private static void setupStudentRepository(String repoUrl, Set<String> subDirs, File reposDir) {
+
+        boolean success = false;
+        int attempts = 3;
+        Exception lastError = null;
+
+        while (!success && attempts > 0) {
+            try {
+                String[] parts = repoUrl.split("/");
+                String studentNick = parts[parts.length - 2];
+                String localRepoDirName = studentNick;
+                File repoDir = new File(reposDir, localRepoDirName);
+                subDirs.add("./repos/" + localRepoDirName);
+                // Проверяем, существует ли репозиторий
+                if (repoDir.exists() && new File(repoDir, ".git").exists()) {
+                    try (Git git = Git.open(repoDir)) {
+                        System.out.println("Pulling repository: " + repoDir.getName());
 
 
-                            git.fetch().call();
-                            git.reset().setMode(ResetCommand.ResetType.HARD).setRef("origin/main").call();
-                            System.out.println("Pulled with success");
-                            break;
-//                            if (result.isSuccessful()) {
-//                                System.out.println("Successfully pulled: " + repoDir.getName());
-//                                System.out.println("Fetch result: " + result.getFetchResult());
-//                                System.out.println("Merge result: " + result.getMergeResult());
-//                                continue;
-//                            } else {
-//                                System.out.println("Unsuccess in pull for " + repoDir.getName());
-//                            }
-//                            success = true;
+                        git.fetch().call();
+                        git.reset().setMode(ResetCommand.ResetType.HARD).setRef("origin/main").call();
+                        System.out.println("Pulled with success");
+                        break;
 
-                        } catch (Exception e) {
-                            System.out.println("Pull failed for: " + repoDir.getName());
-                        }
-                    } else if (repoDir.exists()) {
-                        System.out.println("There is no repo in this folder");
-                        repoDir.delete();
+                    } catch (Exception e) {
+                        System.out.println("Pull failed for: " + repoDir.getName());
                     }
-
-                    // Если репозитория нет, клонируем
-                    System.out.println("Repository " + repoDir.getName() + " not found. Cloning...");
-                    try (Git git = Git.cloneRepository()
-                            .setURI(repoUrl)
-                            .setDirectory(repoDir)
-                            .call()) {
-                        System.out.println("Repo " + repoUrl + " successfully downloaded");
-                        success = true;
-                    }
-
-
-//                    if (success) {
-//                        try {
-//                            encodingToUtf8(repoDir);
-//                        } catch (FileNotFoundException e) {
-//                            System.out.println("Encoding check failed for: " + repoDir.getName());
-//                        }
-//                    }
-                } catch (TransportException e) {
-                    attempts--;
-                    lastError = e;
-                    if (attempts > 0) {
-                        System.out.println("Connection error. Retrying (" + attempts + " attempts left)...");
-                    } else {
-                        System.out.println("Failed to download repo: " + repoUrl);
-                        e.printStackTrace();
-                    }
-                } catch (GitAPIException e) {
-                    lastError = e;
-                    System.out.println("Error processing repo: " + repoUrl);
-                    e.printStackTrace();
-                    break;
+                } else if (repoDir.exists()) {
+                    System.out.println("There is no repo in this folder");
+                    repoDir.delete();
                 }
-            }
 
-            if (!success && lastError != null) {
+                // Если репозитория нет, клонируем
+                System.out.println("Repository " + repoDir.getName() + " not found. Cloning...");
+                try (Git git = Git.cloneRepository()
+                        .setURI(repoUrl)
+                        .setDirectory(repoDir)
+                        .call()) {
+                    System.out.println("Repo " + repoUrl + " successfully downloaded");
+                    success = true;
+                }
+
+            } catch (TransportException e) {
+                attempts--;
+                lastError = e;
+                if (attempts > 0) {
+                    System.out.println("Connection error. Retrying (" + attempts + " attempts left)...");
+                } else {
+                    throw new DSLException("Failed to download repo: " + repoUrl);
+                }
+            } catch (GitAPIException e) {
                 throw new DSLException("Failed to download: " + repoUrl);
             }
         }
-        return subDirs;
+        if (!success && lastError != null) {
+            throw new DSLException("Failed to download: " + repoUrl);
+        }
     }
 
 //    private static Set<String> encodingToUtf8(File directoryRoot) throws IOException {
@@ -127,28 +106,32 @@ public class Downloader {
 
     public static Set<String> downloadAdditional(List<String> sources) {
         Set<String> subDirs = new HashSet<>();
-        int index = 0;
-        for (String source: sources) {
-            if (source.contains("https://github.com/")) {
-                setupGithubRepo(source, "./repos/" + index);
-//            } else if (source.contains("https://") || sources.contains("http://")) {
-//                setupRemoteHttpResource(source, "./repos/" + index);
-            } else {
-                setupLocalResource(source, "./repos/" + index);
-            }
-            subDirs.add("./repos/" + index);
-            index++;
-        }
+
+        sources.parallelStream().forEach(
+                source -> {
+                    boolean created = true;
+                    if (source.contains("https://github.com/")) {
+                        if (new File("./repos/" + source.split("/")[3]).exists()) {
+                            created = false;
+                        } else {
+                            created = setupGithubRepo(source, "./repos/" + source.split("/")[3]);
+                        }
+                    } else {
+                        created = setupLocalResource(
+                                source,
+                                "./repos/" + source.split("/")[sources.size() - 1]
+                        );
+                    }
+                    if (created) {
+                        subDirs.add("./repos/" + source);
+                    }
+                }
+        );
+
         return subDirs;
     }
 
-    private static void setupGithubRepo(String repoUrl, String destinationPath) throws DSLException {
-        String[] parts = repoUrl.split("/");
-        String studentName = parts[3];
-        if (new File("./repos/" + studentName).exists()) {
-            return;
-        }
-
+    private static boolean setupGithubRepo(String repoUrl, String destinationPath) throws DSLException {
         File repoDir = new File(destinationPath);
         boolean success = false;
         int attempts = 3;
@@ -171,6 +154,7 @@ public class Downloader {
                         } catch (Exception exception) {
                             attempts--;
                             System.err.println("[WARNING] Trubles when download repo: " + repoUrl);
+                            success = false;
                         }
                     }
                 } else {
@@ -187,21 +171,23 @@ public class Downloader {
                 System.out.println("Connection error (" + attempts + " attempts left): " + e.getMessage());
                 if (attempts == 0) {
                     System.err.println("[WARNING] Trubles when download repo: " + repoUrl);
+                    success = false;
                 }
             }
         }
+        return success;
     }
 
 //    private static void setupRemoteHttpResource(String link, String destinationPath) {
 //
 //    }
 
-    private static void setupLocalResource(String sourcePath, String destinationPath) {
+    private static boolean setupLocalResource(String sourcePath, String destinationPath) {
         File source = new File(sourcePath);
 
         if (!source.exists()) {
             System.err.println("[WARNING] Cannot access given resource: " + sourcePath);
-            return ;
+            return false;
         }
 
         File target = new File(destinationPath);
@@ -215,6 +201,8 @@ public class Downloader {
             }
         } catch (IOException e) {
             System.err.println("[WARNING] Failed to copy local resource: " + sourcePath);
+            return false;
         }
+        return true;
     }
 }
